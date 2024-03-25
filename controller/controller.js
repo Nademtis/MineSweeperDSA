23c794db91369c23b21b6307cef29b0d09fbd733
MatDat <math03t7@stud.kea.dk>
2024-03-25 11:03:17 +0100

    View startup

--- controller/controller.js
+++ controller/controller.js
@@ -1,17 +1,17 @@
-"use strict"
+"use strict";
 
-import Model from "../model/model.js"
-import View from "../view/view.js"
+import Model from "../model/model.js";
+import View from "../view/view.js";
 
 export default class Controller {
-    constructor() {
-        this.model = new Model(30,16,99)
-        this.view = new View(this)
-    }
-    init() {
-        
-        this.model.initGrid();
-        
-    }
+  constructor() {
+    this.model = new Model(30, 16, 99);
+    this.view = new View(this);
+  }
 
+  init() {
+    this.model.initGrid();
+    const grid = this.model.grid;
+    this.view.displayGrid(this.model.width, this.model.height, grid);
+  }
 }
--- view/view.js
+++ view/view.js
@@ -1,9 +1,27 @@
-"use strict"
+"use strict";
 export default class View {
+  constructor(controller) {
+    this.controller = controller;
+  }
 
-    constructor(controller) {
-        this.controller = controller;
+  displayGrid(rows, cols, grid) {
+    const board = document.querySelector("#boardContainer");
 
+    board.style.setProperty("--GRID_WIDTH", cols);
+
+    for (let row = 0; row < rows; row++) {
+      for (let col = 0; col < cols; col++) {
+        const tile = document.createElement("div");
+
+        this.addImageToDiv(tile, "view/img/tileClosed.png");
+
+        tile.classList.add("tile");
+        board.appendChild(tile);
+      }
     }
-    
+  }
+
+  addImageToDiv(div, imagePath) {
+    div.style.backgroundImage = `url(${imagePath})`;
+  }
 }
